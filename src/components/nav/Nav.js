7952e2766eb1068435d9af42e8593415418be264
kwanele <kwanelegamedze4@gmail.com>
2021-08-16 20:39:55 +0200

    made the navbar

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -5,27 +5,33 @@ import menu from '../../images/icon-hamburger.svg'
 import close from '../../images/icon-close.svg'
 import BtnPrimary from '../btn/btn-primary'
 
-
 function Nav() {
     let closing = useRef(null)
     let listy = useRef(null)
     let open = useRef(null)
 
     function openNav() {
-            listy.style.display= "flex"
-            closing.style.display= "block"
-            open.style.display= "none"
+        listy.style.display= "flex"
+        closing.style.display= "block"
+        open.style.display= "none"
     }
     function closeNav() {
-            listy.style.display = "none"
-            closing.style.display = "none"
-            open.style.display = "block"
+        listy.style.display = "none"
+        closing.style.display = "none"
+        open.style.display = "block"
     }
 
     return (
         <div className="nav">
             <img className="nav__logo" src={ logo } alt="logo"/>
-            <ul ref={(ve)=> {listy = ve}}  className="nav__list">
+            <ul className="nav__list">
+                <li>Pricing</li>
+                <li>Products</li>
+                <li>About Us</li>
+                <li>Careers</li>
+                <li>Community</li>
+            </ul>
+            <ul ref={(ve)=> {listy = ve}}  className="nav__list-mob">
                 <li>Pricing</li>
                 <li>Products</li>
                 <li>About Us</li>
@@ -37,7 +43,7 @@ function Nav() {
             </div>
             <img ref={(el)=> {open = el}} className="nav__menu" src={menu} alt="icon" onClick={ openNav }/>
             <img ref={(el)=> {closing = el}} className="nav__close" src={ close} alt="icon" onClick={ closeNav }/>
-        </div>
+        </div>  
     )
 }
 
